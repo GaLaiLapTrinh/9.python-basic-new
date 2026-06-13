bdbc645d8176653a10b7ed0e0329dbba845e6426
GaLaiLapTrinh <galailaptrinh@gmail.com>
2026-06-13 15:38:53 +0700

    Initial commit

---
+++ chuong_1/bai1.4.py
@@ -0,0 +1,9 @@
+# 1.4 bài tập
+a = int(input("Nhập số a: "))
+b = int(input("Nhập số b: "))
+print(a+b)
+print(a, "+", b, "=", a+b)
+print(a, "-", b, "=", a-b)
+print(a, "*", b, "=", a*b)
+print(type(a))
+print(type(b))
---
+++ chuong_1/bai1.py
@@ -0,0 +1,18 @@
+# Học bài 1 python
+print('Xin chào, tôi đang học python')
+print("Hôm nay là ngày đẹp trời")
+print('Python',"rat",'thu vi')
+print('Tôi là' + ' ' +  "Minh")
+
+#bài 3: nhập lời chúc
+ngay_le = input("Nhập ngày lễ: ")
+nguoi_gui= input("Nhập tên của bạn: ")
+nguoi_nhan = input("Nhập tên người bạn muốn chúc: ")
+
+#hiển thị lời chúc
+print("--" *20)
+print('Nhân dịp ' + ngay_le)
+print('Lời chúc từ ' + nguoi_gui)
+print('Chúc ' + nguoi_nhan + ' có dịp nghỉ lễ ' + 'tốt lành!')
+
+
